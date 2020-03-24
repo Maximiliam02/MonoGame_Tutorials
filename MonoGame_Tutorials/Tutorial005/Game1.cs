6ddc8fe1b53cf2d298771cfe46dd76d7e85b7177
Maximiliam Schylander <54435275+Maximiliam02@users.noreply.github.com>
2020-03-24 13:29:07 +0100

    Bilden kan nu rotera

--- MonoGame_Tutorials/Tutorial005/Game1.cs
+++ MonoGame_Tutorials/Tutorial005/Game1.cs
@@ -1,92 +1,130 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
-namespace Tutorial005
+namespace Template
 {
-  /// <summary>
-  /// This is the main type for your game.
-  /// </summary>
-  public class Game1 : Game
-  {
-    GraphicsDeviceManager graphics;
-    SpriteBatch spriteBatch;
-
-    private Sprite _sprite;
-
-    public Game1()
-    {
-      graphics = new GraphicsDeviceManager(this);
-      Content.RootDirectory = "Content";
-    }
-
     /// <summary>
-    /// Allows the game to perform any initialization it needs to before starting to run.
-    /// This is where it can query for any required services and load any non-graphic
-    /// related content.  Calling base.Initialize will enumerate through any components
-    /// and initialize them as well.
+    /// This is the main type for your game.
     /// </summary>
-    protected override void Initialize()
+    public class Game1 : Game
     {
-      // TODO: Add your initialization logic here
+        GraphicsDeviceManager graphics;
+        SpriteBatch SpriteBatch;
+        
 
-      base.Initialize();
+        Texture2D Skepp;
+        Texture2D Bakgrund;
+        Texture2D Asteroider;
+        Texture2D Mynt;
+
+        Vector2 SkeppPos = new Vector2(100, 300);
+        //center av skeppet
+        Vector2 SkeppStart;
+        Vector2 Asteroid;
+        Vector2 BakgrundPos;
+
+        Rectangle SkeppRektangle;
+
+        float Rotera;
+        float RoteraFart = 5f;
+
+
+
+
+
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+            Bakgrund = Content.Load<Texture2D>("Bakgrund");
+            Skepp = Content.Load<Texture2D>("Skepp");
+
+
+
+
+
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+
+        protected override void Update(GameTime gameTime)
+        {
+            KeyboardState kNewstate = Keyboard.GetState();
+            KeyboardState kstate = Keyboard.GetState();
+            if (kstate.IsKeyDown(Keys.Right))
+                Rotera += MathHelper.ToRadians(RoteraFart);
+            if (kstate.IsKeyDown(Keys.Left))
+                Rotera -= MathHelper.ToRadians(RoteraFart);
+            if (kstate.IsKeyDown(Keys.Down))
+                SkeppPos.Y++;
+            if (kstate.IsKeyDown(Keys.Up))
+                SkeppPos.Y--;
+
+            SkeppRektangle = new Rectangle((int)SkeppPos.X, (int) SkeppPos.Y, Skepp.Width, Skepp.Height);
+            SkeppStart = new Vector2(SkeppRektangle.Width / 2, SkeppRektangle.Height / 2);
+
+
+            
+
+            base.Update(gameTime);
+        }
+
+
+        // TODO: Add your update logic here
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            graphics.ApplyChanges();
+            SpriteBatch.Begin();
+            SpriteBatch.Draw(Bakgrund, BakgrundPos, Color.White);
+            SpriteBatch.Draw(Skepp, SkeppPos, null, Color.White, Rotera, SkeppStart, 1, SpriteEffects.None, 0f);
+            SpriteBatch.End();
+
+        }
     }
 
-    /// <summary>
-    /// LoadContent will be called once per game and is the place to load
-    /// all of your content.
-    /// </summary>
-    protected override void LoadContent()
-    {
-      // Create a new SpriteBatch, which can be used to draw textures.
-      spriteBatch = new SpriteBatch(GraphicsDevice);
 
-      var texture = Content.Load<Texture2D>("Sprite");
 
-      _sprite = new Sprite(texture)
-      {
-        Position = new Vector2(100, 100),
-        Origin = new Vector2(texture.Width / 2, texture.Height - 25),
-      };
-    }
-
-    /// <summary>
-    /// UnloadContent will be called once per game and is the place to unload
-    /// game-specific content.
-    /// </summary>
-    protected override void UnloadContent()
-    {
-      // TODO: Unload any non ContentManager content here
-    }
-
-    /// <summary>
-    /// Allows the game to run logic such as updating the world,
-    /// checking for collisions, gathering input, and playing audio.
-    /// </summary>
-    /// <param name="gameTime">Provides a snapshot of timing values.</param>
-    protected override void Update(GameTime gameTime)
-    {
-      _sprite.Update();
-
-      base.Update(gameTime);
-    }
-
-    /// <summary>
-    /// This is called when the game should draw itself.
-    /// </summary>
-    /// <param name="gameTime">Provides a snapshot of timing values.</param>
-    protected override void Draw(GameTime gameTime)
-    {
-      GraphicsDevice.Clear(Color.CornflowerBlue);
-
-      spriteBatch.Begin();
-
-      _sprite.Draw(spriteBatch);
-
-      spriteBatch.End();
-
-      base.Draw(gameTime);
-    }
-  }
 }
